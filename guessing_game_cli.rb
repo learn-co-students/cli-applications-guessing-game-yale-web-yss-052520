54ec988111cf75aa441c8ee5f76e56e4b2191740
Chad Palmer <chad.palmer@yale.edu>
2020-05-19 22:09:43 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,12 @@
-# Code your solution here!
+def run_guessing_game
+  n = rand(6) + 1
+  guess = gets.chomp
+  case guess
+    when n.to_s 
+      puts "You guessed the correct number!"
+    when "exit"
+      puts "Goodbye!"
+    else
+      puts "Sorry! The computer guessed #{n}."
+    end
+end
